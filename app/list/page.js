2f34d09a386d875cb7b52fe089e82fc3c3e23c4b
dpound@naver.com <dpound@naver.com>
2023-10-18 05:50:08 +0900

    연습용 next-js 연동완료, 세팅완료

---
+++ app/list/page.js
@@ -0,0 +1,16 @@
+const list = () => {
+    return (
+    
+    <div>
+        <div className="shopping-item">
+            <h5>노트북 $100</h5>
+        </div>
+        <div className="shopping-item">
+            <h5>노트북 $100</h5>
+        </div>
+    </div>
+    )
+
+}
+
+export default list;
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h4>ㅎㅇㅎㅇ</h4>
+    </div>
+  )
+}
